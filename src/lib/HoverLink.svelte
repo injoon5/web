c1aa86843ca004c04f217bb53b3782765bf0804d
Injoon Oh <injoon5@naver.com>
2024-03-03 23:56:32 +0900

    Hoverlink inline-block

--- src/lib/HoverLink.svelte
+++ src/lib/HoverLink.svelte
@@ -1,6 +1,6 @@
 <script>
 </script>
 
-<p class="hover:bg-primary-1000 -mx-1 rounded-md px-1 transition">
+<p class="hover:bg-primary-1000 -mx-1 inline-block rounded-md px-1 transition">
 	<slot />
 </p>
