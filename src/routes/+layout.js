2dfcafbc1892526e98987380dac6c447b041fcfe
Injoon Oh <injoon5@naver.com>
2025-07-06 23:50:03 +0900

    Posthog analytics

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -4,7 +4,8 @@ import { browser } from '$app/environment';
 export const load = async () => {
 	if (browser) {
 		posthog.init('phc_bGPHXwctnUvocW2PwQ7bbfWUKDGwmuA2KbhlqGTsh9G', {
-			api_host: 'https://us.i.posthog.com',
+			api_host: '/relay-oij225/',
+			ui_host: 'https://us.posthog.com',
 			defaults: '2025-05-24',
 			person_profiles: 'always' // or 'always' to create profiles for anonymous users as well
 		});
