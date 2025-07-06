0f20dfffa631d627e5c0d02855d92158d1685eb0
Injoon Oh <injoon5@naver.com>
2025-07-06 23:40:52 +0900

    Writing us-camp... final note left

---
+++ src/routes/+layout.js
@@ -0,0 +1,14 @@
+import posthog from 'posthog-js';
+import { browser } from '$app/environment';
+
+export const load = async () => {
+	if (browser) {
+		posthog.init('phc_bGPHXwctnUvocW2PwQ7bbfWUKDGwmuA2KbhlqGTsh9G', {
+			api_host: 'https://us.i.posthog.com',
+			defaults: '2025-05-24',
+			person_profiles: 'always' // or 'always' to create profiles for anonymous users as well
+		});
+	}
+
+	return;
+};
